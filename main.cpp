4d40458a9092386010af806a58314e0037f8cf84
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-03-27 20:43:38 -0300

    soma,subt,mult e div

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ int main (){
 
   int num1 = 8;
   int num2 = 2;
+  int soma, subt, mult, div;
 
-  std::cout<<num1 + num2<<std::endl;
-  std::cout<<num1 - num2<<std::endl;
-  std::cout<<num1 * num2<<std::endl;
-  std::cout<<num1 / num2<<std::endl;
-
+  soma = num1 + num2;
+  subt = num1 - num2;
+  mult = num1 * num2;
+  div = num1 / num2;
+  
+  std::cout<<soma<<std::endl;
+  std::cout<<subt<<std::endl;
+  std::cout<<mult<<std::endl;
+  std::cout<<div<<std::endl;
 
     return 0;
 }
