1d70061c8ac4dde0a2ed9177d873b3da97ec6705
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-03-27 20:18:48 -0300

    std, cout

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include<iostream>
-#include<iomanip>
+
 
 int main (){
 
-    int a = 8;
-    //int número inteiro
-    float b = 9.6;
-    //float número de ponto flutuante ocupa 4bytes.
-    double c = 8.6952132545;
-    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
-    //double ocupa 8bytes.
-
-
-    std::cout<<a<<std::endl;
-    std::cout<<b<<std::endl;
-    std::cout<<c<<std::endl;
-
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
+    
     return 0;
 }
