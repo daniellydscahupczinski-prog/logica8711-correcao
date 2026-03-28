028ca6e95f75d08bb45c0f9fde55903a046a4349
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-03-27 21:58:37 -0300

    Perguntas pessoais

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@
 int main (){
 
   std::string nome;
+  int idade;
+  float altura;
 
   std::cout<<"Qual seu nome: "<<std::endl;
   std::cin>>nome;
+  std::cout<<"Qual sua idade: "<<std::endl;
+  std::cin>>idade;
+  std::cout<<"Qual sua altura: "<<std::endl;
+  std::cin>>altura;
 
   std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+  std::cout<<"Voce tem "<<idade<<" anos"<<std::endl;
+  std::cout<<"Voce tem "<<altura<< " metros"<<std::endl;
 
 
 
