104379b155117f7b80a7a732908cae4b91a23791
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-03-30 20:46:00 -0300

    sinais de diferença

--- main.cpp
+++ main.cpp
@@ -13,8 +13,12 @@ int b;
   std::cin>>b;
 
   std::cout<<(a > b)<<std::endl;
+  //maior que...
   std::cout<<(a >= b)<<std::endl;
+  //maior ou igual a...
   std::cout<<( a != b)<<std::endl;
-  
+  //diferente de...
+  std::cout<<(a ==b)<<std::endl;
+  //igual a...
     return 0;
 }
