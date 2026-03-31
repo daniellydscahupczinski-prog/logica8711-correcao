635236968801e3ab76e15a4edfbae0ccfda2c8ac
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-03-30 21:48:08 -0300

    +=

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 int main (){
 
   int a = 10;
-  
-  std::cout<<++a<<std::endl;
-  //adicionar mais um numero ao que foi rodado (10+1=11+1=12)
-  std::cout<<++a<<std::endl;
+
+  a += 5;
+  //a = a + 5
+
+  std::cout<<a<<std::endl;
 
     return 0;
 }
