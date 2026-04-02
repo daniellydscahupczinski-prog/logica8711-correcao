ec6373e564430b925694ee8dcc11eedfced63c4b
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-01 21:57:15 -0300

    string nome

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
-
+#include<string>
 
 int main (){
 
-  int num1, num2, soma;
+  std::string nome = "Danielly";
+  std::string sobrenome = "Cahupczinski";
 
-  std::cout<<"Digite num 1: "<<"\n";
-  std::cin>>num1;
+ std::cout<<nome + " " + sobrenome<<std::endl;
 
-  std::cout<<"Digite num 2: "<<"\n";
-  std::cin>>num2;
-
-  soma = num1 + num2;
-
-  std::cout<<"\n O resultado da soma é "<<soma<<"\n";
-
-  // "\n" serve para dar enter, igual ao endl, porem ocupando menos espaço
-  
     return 0;
 }
