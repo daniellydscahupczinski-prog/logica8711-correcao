3d9b6ef3d0c2ff609d237447f07cc87dce706e7a
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-08 21:01:25 -0300

    sqrt, round e log

--- main.cpp
+++ main.cpp
@@ -4,14 +4,11 @@
 int main (){
 
   int a = 5;
-  int b = 3;
-  int c = 5;
-  int d = 2;
-
-  int maior1 = std::max(a, b);
-  int maior2 = std::max(c, d);
-
-  std::cout<<(maior1 > maior2)<<std::endl;
-
+std::cout<<std::sqrt(64)<<std::endl;
+// o resultado é a raiz quadrada
+std::cout<<std::round(2.6)<<std::endl;
+// o resultado é arredondado
+std::cout<<std::log(2)<<std::endl;
+//da um numero quebrado
     return 0;
 }
