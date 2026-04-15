c508fcfe041e61a18ba201d69f3c5e347bda89cd
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-15 19:13:53 -0300

    loop com o for

--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
 #include<iostream>
-#include<string>
 
 
 int main (){
  
+  for(int i = 1; i <=10; i++){
+      std::cout<<"Este e um loop!!"<<std::endl;
+  }
 
-std::string nome;
-float altura;
-int idade;
-
-std::cout<<"Digite seu nome"<<std::endl;
-std::cin>>nome;
-
-
-std::cout<<"Digite sua altura"<<std::endl;
-std::cin>>altura;
-
-std::cout<<"Digite sua idade"<<std::endl;
-std::cin>>idade;
-
-
-if(idade <= 10)
-{std::cout<<"voce se encontra na classificaçao infantil"<<idade<<std::endl;
-}else if (idade <= 18)
-
-{std::cout<<"voce se encontra na classificaçao adolecente"<<idade<<std::endl;
-}else if(idade <= 60)
-
-{std::cout<<"Voce se encontra na classificaçao adulta"<<idade<<std::endl;
-}else{
-  std::cout<<"Voce se encontra na classificaçao senior!"<<std::endl;
-}
   return 0;
-
   }
