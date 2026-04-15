9b0ef38cdd6ada773f89832c5418dc2b89776a46
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-15 19:45:01 -0300

    sleep for

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
-
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> //para definir unidade de tempo (ms, segundos)
 
 int main (){
  
   for(int i = 10; i >= 0; i--){
       std::cout<<"A bomba ira explodir em>>"<<i<<std::endl;
+
+       std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 
   return 0;
