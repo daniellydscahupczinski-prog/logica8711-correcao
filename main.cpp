8a0d88698224d1020ca8b780169861e0f023378a
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-15 20:19:00 -0300

    switch com dias da semana

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
-#include<iostream>
+#include <iostream>
 
-int main (){
+int main()
+{
 
-  int tabuada = 7;
+  int opcao;
 
-  for(int i =0; i <=10; i++){
-    std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+  std::cout << "Digite um valor para opcao: " << std::endl;
+  std::cin >> opcao;
+
+  switch (opcao)
+  {
+  case 1:
+    std::cout << "Domingo!" << std::endl;
+    break;
+  case 2:
+    std::cout << "Segunda-feira" << std::endl;
+    break;
+  case 3:
+    std::cout << "Terça-feira" << std::endl;
+    break;
+  case 4:
+    std::cout << "Quarta-feira" << std::endl;
+    break;
+  case 5:
+    std::cout << "Quinta-feira" << std::endl;
+    break;
+  case 6:
+    std::cout << "Sexta-feira" << std::endl;
+    break;
+  case 7:
+    std::cout << "sabado" << std::endl;
+    break;
+
+  default:
+    std::cout << "numero invalido!" << std::endl;
   }
- 
-  
 
   return 0;
-  }
+}
