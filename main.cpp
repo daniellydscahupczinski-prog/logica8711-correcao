54bb3c2f9712bd220538f41781ff7b7680a8bf09
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-15 19:58:38 -0300

    tabuada feita com o (for)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
-#include<thread> //para lidar com o tempo de execução
-#include<chrono> //para definir unidade de tempo (ms, segundos)
 
 int main (){
- 
-  for(int i = 10; i >= 0; i--){
-      std::cout<<"A bomba ira explodir em>>"<<i<<std::endl;
 
-       std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  int tabuada = 7;
+
+  for(int i =0; i <=10; i++){
+    std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
   }
+ 
+  
 
   return 0;
   }
