852600eeeb88c37d90f7dd1a74d739d98f1623d6
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-15 21:12:54 -0300

    soma com o for

--- main.cpp
+++ main.cpp
@@ -1,40 +1,16 @@
 #include <iostream>
 
-int main()
-{
+int main(){
+  int soma = 0;
 
-  int opcao;
+  for(int i = 1; i <= 5; i++){
+    
+    soma = soma + i;
+  
+   }
 
-  std::cout << "Digite um valor para opcao: " << std::endl;
-  std::cin >> opcao;
-
-  switch (opcao)
-  {
-  case 1:
-    std::cout << "Domingo!" << std::endl;
-    break;
-  case 2:
-    std::cout << "Segunda-feira" << std::endl;
-    break;
-  case 3:
-    std::cout << "Terça-feira" << std::endl;
-    break;
-  case 4:
-    std::cout << "Quarta-feira" << std::endl;
-    break;
-  case 5:
-    std::cout << "Quinta-feira" << std::endl;
-    break;
-  case 6:
-    std::cout << "Sexta-feira" << std::endl;
-    break;
-  case 7:
-    std::cout << "sabado" << std::endl;
-    break;
-
-  default:
-    std::cout << "numero invalido!" << std::endl;
-  }
+  std::cout<<"Soma é: "<<soma<<std::endl;
+ 
 
   return 0;
 }
