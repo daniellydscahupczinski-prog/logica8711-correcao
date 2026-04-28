292c857877e119630c24518b5b8be69d45155b62
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-28 20:18:11 -0300

    void

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-
-int main(){
     
-    int myNumebers[5] = {1, 2, 3, 4, 5};
-
-    //for(int i = 0; i < 5; i++){
-        //std::cout<<myNumebers[i]<<std::endl;}
+    void myFunction(){
+        std::cout<<"Ola mundo!"<<std::endl;
+    }
     
-    for(int num : myNumbers){
-    std::cout<<num<<std::endl;}
+    int main (){
+        myFunction();
     
     return 0;
 }
