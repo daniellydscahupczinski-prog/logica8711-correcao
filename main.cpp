ee40fa3fd3a6ad1dd775f3fe32fc27aaaac8c386
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-04-30 19:19:40 -0300

    mais exemplos de if e else

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <string>
 
-int doubleGame(int x){
-    return x * 2;
-}
-    
-    int main(){
+int main(){
 
-        for(int i = 1; i <=5; i++){
-            std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
-        }
+    int opçao;
+    std::string nome;
+    int idade;
+
+    std::cout<<"---- Escolha a opçao adequada "<<std::endl;
+    std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
+    std::cin>>opçao;
+
+    if(opçao == 1){
+        std::cout<<"Digite o nome: "<<std::endl;
+        std::cin>>nome;
+        std::cout<<"Digite a idade:"<<std::endl;
+        std::cin>>idade;
+
+        std::cout<<"Seu nome é: "<<nome<<std::endl;
+        std::cout<<"Sua idade é: "<<idade<<std::endl;
+
+    }else{
+        std::cout<<"Ate a proxima!"<<std::endl;
+ }
     
     return 0;
 }
