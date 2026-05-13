5334851a91843a4afc09c31ebcf8030fcea0d9ab
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-05-12 21:59:13 -0300

    uso do While

--- main.cpp
+++ main.cpp
@@ -1,67 +1,17 @@
 #include<iostream>
 
-double saldo = 1000.0;
-std::string titular = "Danielly";
-
-void exibirSaldo(){
-    std::cout<<titular<<" --- Saldo: R$ "<<saldo<<std::endl;
-}
-
-void depositar(double valor){
-    if(valor > 0){
-        saldo += valor;
-        std::cout<<"Deposito realizado com sucesso"<<std::endl;
-    }else{
-        std::cout<<"Valor invalido"<<std::endl;
-    }
-}
-
-void sacar(double valor){
-    if(valor > 0 && valor <= saldo){
-        saldo -= valor;
-        std::cout<<"Saldo insuficiente ou valor inválido!"<<std::endl;
-    }
-}
-void exibirMenu(){
-    std::cout<<"\n === MENU CONTA BANCARIA === "<<std::endl;
-    std::cout<<"1 - Ver saldo"<<std::endl;
-    std::cout<<"2 -Deposito"<<std::endl;
-    std::cout<<"3 - Sacar"<<std::endl;
-    std::cout<<"4 - Sair"<<std::endl;
-    std::cout<<"Digite uma opção ";
-}
-
 int main(){
 
-    int opcao;
-    double valor;
+    int numero, sorteado = 42;
 
-    do{
-        exibirMenu();
-        std::cin>>opcao;
-        
-        switch(opcao){
-            case 1:
-            exibirSaldo();
-            break;
-            case 2:
-            std::cout<<"Digite o valor a depositar: "<<std::endl;
-            std::cin>>valor;
-            depositar(valor);
-            break;
-            case 3:
-            std::cout<<"Digite o valor a sacar: ";
-            std::cin>>valor;
-            break;
-            case 4:
-            std::cout<<"Saindo da conta.."<<std::endl;
-            break;
-            default:
-            std::cout<<"Opção invalida"<<std::endl;
-
-        }
-
-    }while(opcao != 4);
+        do{
+            std::cout<<"Adivinhe: ";
+            std::cin>>numero;
+            if(numero < sorteado) std::cout<<"Maior!"<<std::endl;
+            else if(numero > sorteado)std::cout<<"Menor!"<<std::endl;
+            }while(numero != sorteado);
+            
+        std::cout<<"Acertou!!"<<std::endl;
 
         return 0;
 }
