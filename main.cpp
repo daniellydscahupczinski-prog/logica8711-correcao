63a9f7444535d4149ff821f728f1ae859371eff5
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-05-26 21:36:05 -0300

    uso do arr

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 
-int main (){
+int encontrarMaior(int arr[], int tamanho){
+    int maior = arr[0];
 
+    for(int i= 1; i < tamanho; i++){
+        if(arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return maior;
+}
+
+int main(){
     int numeros[5];
-    int soma = 0;
 
-    for(int i = 0; i <= 4; i++){ 
-    std::cout<<"Digite o numero: "<<(i +1)<<": ";
-    std::cin>>numeros[i];
-    soma += numeros[i];
- }
+    std::cout<<"Digite 5 numeros "<<std::endl;
+        for(int i = 0; i < 5; i++){
+            std::cout<<"Digite o numero "<<(i + 1)<<": ";
+            std::cin>>numeros[i];
+        }
 
-    std::cout<<"Soma: "<<soma<<std::endl;
+        std::cout<<"Maior numero: "<<encontrarMaior(numeros, 5)<<std::endl;
+
+        return 0;
 }
