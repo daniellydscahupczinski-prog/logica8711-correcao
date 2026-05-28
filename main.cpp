b91c8c0eabb46841aff79280638f9ffe5266a716
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-05-28 20:28:55 -0300

    fibonacci com tabela em codigo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 
 int fibonacci(int n){
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    std::cout<<"Calculando fib("<<n<<")"<<std::endl;
 
-    return fibonacci(n - 1) + fibonacci (n - 2);
-}
-    int main(){
-        std::cout<<fibonacci(3)<<std::endl;
-
+    if(n == 0){
+        std::cout<<"Fib(0) = o (parada)"<<std::endl;
         return 0;
     }
+    if(n == 1){
+        std::cout<<"Fib(1) = 1 (parada)"<<std::endl;
+        return 1;
+    }
+    std::cout<<"Fib("<<n<<") = Fib("<<(n-1)<<") + fib("<<(n-2)<<")\n";
+    int resultado = fibonacci(n - 1) + fibonacci(n - 2);
+    std::cout<<"Fib("<<n<<") = "<< resultado << "\n";
+    return resultado;
+}
+int main(){
+
+    fibonacci(4);
+
+
+    return 0;
+}
