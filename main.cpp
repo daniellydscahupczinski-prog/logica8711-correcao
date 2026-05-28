380ef9dcf669b3cd5c089dbcfbc2951d95e49c0d
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-05-28 19:11:17 -0300

    tuple

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
 #include<iostream>
+#include<tuple>
+#include<string>
 
-bool ehPrimo(int numero){ 
-    if(numero <= 1){
-        return false;
-    }
-for(int i = 2; i < numero; i++){
-    if(numero % i == 0){
-        return false;
-    }
-}
+int main(){
 
-return true;
+    std::tuple<int, std::string, double > aluno(1, "Danielly", 9.5);
 
-}
+    std::cout<<"ID: "<<std::get<0>(aluno)<<"\n";
+    std::cout<<"Nome: "<<std::get<1>(aluno)<<"\n";
+    std::cout<<"Nota: "<<std::get<2>(aluno)<<"\n";
 
-int main (){
-    int numero;
-
-    std::cout<<"Digite um numero: "<<std::endl;
-    std::cin>>numero;
-
-    if(ehPrimo(numero)){
-        std::cout<<numero<<"é primo!"<<std::endl;
-    }else{
-        std::cout<<numero<<"Não é primo!"<<std::endl;
-    }
-
-    return 0;
+    return 0; 
 }
