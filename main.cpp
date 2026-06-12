afaf1972bdbea042ea79955b375d440b0d49130c
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-06-11 22:01:07 -0300

    jogo da forca

--- main.cpp
+++ main.cpp
@@ -1,14 +1,64 @@
 #include<iostream>
-//algoritmo de Horner
-int horner(int * coeficientes, int grau, int x){
-    int resultado = coeficientes[grau];
-    for(int i = grau - 1; i >= 0; i--){
-        resultado = resultado * x + coeficientes[i];
-    }
-    return resultado;
-}
+#include<string>
+#include<cctype>
+
 int main(){
-    int coef[] = {5, 4, 3, 2,};
-    std::cout<<"P(2) = "<<horner(coef, 3, 2)<<std::endl;
+    std::string palavra = "programaçao";
+    std::string advinhada = " ";
+
+    int erros = 0;
+    int maxErros = 6;
+    bool ganhou = false;
+
+    for(int i = 0; i < palavra.length(); i++ ){
+        advinhada += '_';
+    }
+    std::cout<<"==== JOGO DA FORCA ====="<<std::endl;
+    std::cout<<"Adivinhe a palavra"<<std::endl;
+    std::cout<<std::endl;
+
+    while(erros < maxErros && "ganhou"){ 
+        std::cout<<"Palavra: ";
+        for(int i = 0; i < advinhada.length(); i++){
+    }
+    std::cout<<std::endl;
+    std::cout<<"Erros: "<<erros<<"/"<<maxErros<<std::endl;
+    std::cout<<std::endl;
+
+    char letra;
+    std::cout<<"Digite uma letra:";
+    std::cin>>letra;
+    letra = std::tolower(letra);
+
+    std::cout<<std::endl;
+
+    bool encontrou = false;
+
+    for(int i = 0; i < palavra.length(); i++){
+        if(palavra[i] == letra){
+            advinhada[i] = letra;
+            encontrou = true;
+             }
+        }
+        if(!encontrou){
+            std::cout<<"Letra errada! "<<std::endl;
+            erros++;
+        }else{
+            std::cout<<"Letra encontrada!"<<std::endl;
+        }
+        std::cout<<std::endl;
+
+        if(advinhada == palavra){
+            ganhou = true;
+        }
+    }
+    std::cout<<"========"<<std::endl;
+    if(ganhou){
+        std::cout<<"Parabens voce venceu"<<std::endl;
+        std::cout<<"A palavra era: "<<palavra<<std::endl;
+    }else{
+        std::cout<<"Game over voce perdeu"<<std::endl;
+        std::cout<<"A palavra era: "<<palavra<<std::endl;
+    }
     return 0;
 }
