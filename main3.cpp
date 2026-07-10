98d691a8b8765e1107552a6a750be2226245dd0d
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-07-09 21:47:42 -0300

    Array para exibir 5 numeros, e Array para exibir a ultima palavra

--- main3.cpp
+++ main3.cpp
@@ -1,34 +1,18 @@
 #include<iostream>
-#include<vector>
 
 int main(){
-    std::vector<int>numeros;
+int numeros [] = {1,2,3,4,5};
+std::cout<<"==== 5 numeros exibidos ===="<<std::endl;
+for (int i = 0; i < 5; i++){
+    std::cout<<numeros[i]<<std::endl;
+}
 
-    std::cout<<"==== Maior e menor ===="<<std::endl;
+std::string palavra [] = { "casa","sofa","teve","mesa","computador"};
+std::cout<<"==== A ULTIMA PALAVRA É ====="<<std::endl;
+std::cout<<palavra[4]<<std::endl;
 
-    numeros.push_back(15);
-    numeros.push_back(8);
-    numeros.push_back(25);
-    numeros.push_back(3);
-    numeros.push_back(42);
 
-    std::cout<<"Vector: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
-    std::cout<<std::endl;
-    int maior = numeros[0];
-    int menor = numeros[0];
-
-    for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] > maior){
-            maior = numeros[i];
-        }
-        if(numeros[i] < menor){
-            menor = numeros[i];
-        }
-    }
-    std::cout<<"Maior: "<<maior<<std::endl;
-    std::cout<<"Menor: "<<menor<<std::endl;
     
-    return 0;}
+return 0;
+    
+}
