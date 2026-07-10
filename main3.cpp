8feb70b251824d6aa5675dd6dd8a91570d55b443
DANIELLY DOS SANTOS CAHUPCZINSKI <05430870005@senacrs.edu.br>
2026-07-10 19:49:48 -0300

    Soma dos numeros com while

--- main3.cpp
+++ main3.cpp
@@ -1,18 +1,24 @@
-#include<iostream>
+#include<iostream> 
 
 int main(){
-int numeros [] = {1,2,3,4,5};
-std::cout<<"==== 5 numeros exibidos ===="<<std::endl;
-for (int i = 0; i < 5; i++){
-    std::cout<<numeros[i]<<std::endl;
-}
 
-std::string palavra [] = { "casa","sofa","teve","mesa","computador"};
-std::cout<<"==== A ULTIMA PALAVRA É ====="<<std::endl;
-std::cout<<palavra[4]<<std::endl;
+  int numero;
+  int soma = 0;
 
+  while(true){
+    std::cout<<"Digite um numero para somar(0 para parar)";
+    std::cin>>numero;
+  
+  if(numero ==0){
+    break;
+  }
 
-    
-return 0;
-    
+  if (numero < 0){
+    std::cout<<"O numero não pode ser negativo, tente outra vez"<<std::endl;
+  }
+  soma = soma + numero;
+  }
+  std::cout<<"Soma :"<<soma<<std::endl;
+
+  return 0;
 }
